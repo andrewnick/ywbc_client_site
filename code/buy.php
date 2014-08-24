c23ee6ef545158f741425ecf11f0c50bb4657bb1
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2014-08-25 09:50:42 +1200

    removed white space and indented nested elements

--- code/buy.php
+++ code/buy.php
@@ -4,11 +4,9 @@ require_once('includes/home_head.php');
 
 ?>
 
-
-	
 	<div class="row row_buy_1 buy clearfix">
 		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-offset-2">
-		<h1>Now available worldwide <br>via Amazon!</h1>
+			<h1>Now available worldwide <br>via Amazon!</h1>
 		</div>
 	</div> 
 
@@ -27,15 +25,11 @@ require_once('includes/home_head.php');
 		</div>
 	</div> 
 
-
 	<div class="row row_buy_4 buy clearfix">
-	<div class="col-xs-8 col-xs-offset-2 embed-responsive embed-responsive-16by9">
-	<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25541.265557504103!2d174.7644895509009!3d-36.85066131977769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47fb5a9ce6fb%3A0x500ef6143a29917!2sAuckland!5e0!3m2!1sen!2snz!4v1408599696589"></iframe>
+		<div class="col-xs-8 col-xs-offset-2 embed-responsive embed-responsive-16by9">
+			<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25541.265557504103!2d174.7644895509009!3d-36.85066131977769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47fb5a9ce6fb%3A0x500ef6143a29917!2sAuckland!5e0!3m2!1sen!2snz!4v1408599696589"></iframe>
+		</div>
 	</div>
-	</div>
-
-
- 
 
  <?php
 
